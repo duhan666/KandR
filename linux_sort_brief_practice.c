9a73526f08ca3ad8b85aa22e959d9b98381be364
duhan666 <kiger-666@163.com>
2017-01-09 17:07:21 +0800

    201701091707

--- linux_sort_brief_practice.c
+++ linux_sort_brief_practice.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
-#define LEN 50
+#include <string.h>
+#include <malloc.h>
 
-void getline(char *s){
-    int c;
-    while( (c=getchar())!=EOF){
+#define LEN 50
+#define MAX 20
+
+
+
+int getline(char *s){
+    int c,counter=0;
+    while( (c=getchar())!='\n'){
         *s++=c;
+        counter++;
+    }
     *s = '\0';
+    return counter;
+}
+
+void mystrcpy(char *p, char *q){
+    while((*(p++)=*(q++))!='\0')
+    *p = '\0';
+}
+
+void readlines(char *s[]){
+    char p[LEN];
+    int ctr;
+    while( ctr=getline(p) ){
+        char *ptr = (char *)malloc(strlen(p));
+        mystrcpy(ptr,p);
+        *s++ = ptr;
     }
 }
 
-
-
-
 void main(){
     char s[LEN];
-    getline(s);
+    int c = getline(s);
+    printf("%d\n",c);
 }
 
