f3e60809ff1f9aa2a1bc48bfcdf6bede0fdd7294
duhan666 <kiger-666@163.com>
2017-01-11 22:55:27 +0800

    201701112255

--- structfront.c
+++ structfront.c
@@ -7,6 +7,13 @@
 int bufp = 0;
 char buf[BUFSIZE];
 
+struct key{
+    char *word;
+    int count;
+}keytab[]={
+    {"auto",0},{"break",0},{"case",0},{"void",0},
+};
+
 int getch(){
     return bufp>0 ? buf[--bufp]:getchar();
 }
@@ -32,7 +39,6 @@ int isalpha(int x){
         return 0;
 }
 
-
 int getword(char *w){
     int c;
 
@@ -56,16 +62,43 @@ int getword(char *w){
     return w[0];
 }
 
-int bisearch(int tar, int v[], int n){
-
+int strcmp(char *p,char *q){
+    while( *p!='\0' && *q!='\0' ){
+        if(*p == *q){
+            p++,q++;
+        }
+        else if(*p > *q){
+            return -1;
+        }
+        else
+            return 1;
+    }
+    return 0;
 }
-
+/*
+int bisearch(char tar[],struct key keytab, int n){
+    int low,mid,high;
+    low = 0;
+    high = n-1;
+    while(low<=high){
+        mid = (low+high)/2;
+        if(v[mid]>tar)
+            high = mid - 1 ;
+        else if(v[mid]<tar)
+            low = mid + 1;
+        else
+            return mid;
+    }
+    return -1;
+}
+*/
 
 
 main(){
-    int v[] = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31};
+/*  int v[] = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31};
     int n = sizeof(v)/sizeof(v[0]);
-    printf("%d\n",n);
+    int c = bisearch(21,v,n);
+    printf("%d\n",c); */
 
 }
 
